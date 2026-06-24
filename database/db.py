26e6ec0aeab9b479d92e441fe097e3f9c656863e
Maximiliano G. Coceres <maximiliano.g.coceres@gmail.com>
2026-06-24 16:30:54 +0200

    correccion de path en db.py

--- database/db.py
+++ database/db.py
@@ -1,11 +1,13 @@
 import pandas as pd 
 import sqlite3
+from pathlib import Path
 
-df = pd.read_csv("ventas_datastore.csv")
+DIR_PATH = Path(__file__).parent 
 
-conn = sqlite3.connect("database.db")
+df = pd.read_csv(DIR_PATH / "ventas_datastore.csv")
+
+conn = sqlite3.connect(DIR_PATH / "database.db")
 
 df.to_sql('ventas', conn, if_exists="replace", index=False)
 
 conn.close()
-
