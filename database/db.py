ea90395b6a8f6bed1c3fd5506fa9e6ad6d157310
Maximiliano Cóceres <maximiliano.g.coceres@gmail.com>
2026-06-29 19:39:03 +0200

    creacion kpis de sql (#10)
    Closes #7

--- database/db.py
+++ database/db.py
@@ -1,13 +1,20 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
 import pandas as pd 
 import sqlite3
 from pathlib import Path
+from utils.cleaning import limpieza_total
 
 DIR_PATH = Path(__file__).parent 
 
-df = pd.read_csv(DIR_PATH / "ventas_datastore.csv")
+df_crudo = pd.read_csv(DIR_PATH / "ventas_datastore.csv")
 
+df_limpio, reporte = limpieza_total(df_crudo)
+print(reporte)
 conn = sqlite3.connect(DIR_PATH / "database.db")
 
-df.to_sql('ventas', conn, if_exists="replace", index=False)
+df_limpio.to_sql('ventas', conn, if_exists="replace", index=False)
 
 conn.close()
